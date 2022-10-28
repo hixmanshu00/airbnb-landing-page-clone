f636d32a2b2a379e9c194007dbadd9c1fa95bb9f
Himanshu Rajput <hiowner00@gmail.com>
2022-10-28 17:59:13 +0530

    added start comp

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Footer from './components/Footer';
 import Hero from './components/Hero';
 import Newsletter from './components/Newsletter';
 import People from './components/People';
+import Start from './components/Start';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Hero />
       <Features />
       <People />
+      <Start />
       <Newsletter />
       <Footer />
     </div>
